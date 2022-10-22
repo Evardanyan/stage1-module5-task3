2d5c91d17b29df1a1b1bdf571cd1912929d8b37f
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-10-22 10:16:58 +0400

    update solution

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        return list.stream().filter(x -> x / 2 == 0).collect(Collectors.toList());
+        return list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
